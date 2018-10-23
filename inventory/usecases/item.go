2eacce555b7db3300a28284c77aa35d14623be2d
Marco Carcano <mc@carcano.ch>
2018-10-23 19:46:38 +0200

    some code cleanup

--- inventory/usecases/item.go
+++ inventory/usecases/item.go
@@ -9,26 +9,11 @@ import (
 	"github.com/go-kit/kit/endpoint"
 )
 
-// For each method, we define request and response structs
 type AddItemRequest struct {
 	Id   string `json:"id"`
 	Name string `json:"name"`
 }
 
-type Response struct {
-	V   string `json:"v"`
-	Err string `json:"err,omitempty"` // errors don't define JSON marshaling
-}
-
-type countRequest struct {
-	S string `json:"s"`
-}
-
-type countResponse struct {
-	V int `json:"v"`
-}
-
-// Endpoints are a primary abstraction in go-kit. An endpoint represents a single RPC (method in our service interface)
 func MakeAddItemEndpoint(svc domain.InventoryInt) endpoint.Endpoint {
 	return func(_ context.Context, request interface{}) (interface{}, error) {
 		req := request.(AddItemRequest)
@@ -40,30 +25,10 @@ func MakeAddItemEndpoint(svc domain.InventoryInt) endpoint.Endpoint {
 	}
 }
 
-func MakeCountEndpoint(svc domain.InventoryInt) endpoint.Endpoint {
-	return func(_ context.Context, request interface{}) (interface{}, error) {
-		req := request.(countRequest)
-		v := svc.Count(req.S)
-		return countResponse{v}, nil
-	}
-}
-
 func DecodeAddItemRequest(_ context.Context, r *http.Request) (interface{}, error) {
 	var request AddItemRequest
 	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
 		return nil, err
 	}
 	return request, nil
-}
-
-func DecodeCountRequest(_ context.Context, r *http.Request) (interface{}, error) {
-	var request countRequest
-	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
-		return nil, err
-	}
-	return request, nil
-}
-
-func EncodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
-	return json.NewEncoder(w).Encode(response)
 }
